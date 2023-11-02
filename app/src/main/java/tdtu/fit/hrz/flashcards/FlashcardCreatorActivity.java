a018dcbe1a24227294c1c88651efd3e1a0c98fbb
harouzie <dophamquanghung@gmail.com>
2023-11-03 00:02:13 +0700

    #dev
    Flashcard basic design
    Question and Answer text
    Question add image

---
+++ app/src/main/java/tdtu/fit/hrz/flashcards/FlashcardCreatorActivity.java
@@ -0,0 +1,52 @@
+package tdtu.fit.hrz.flashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class FlashcardCreatorActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageButton btnAddQuesImg,btnAddAnsImg;
+    private ImageView questionImageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard_creator);
+
+        questionImageView = findViewById(R.id.questionImage);
+        btnAddQuesImg = findViewById(R.id.btnAddQuesImg);
+
+        btnAddQuesImg.setOnClickListener(v -> openImagePicker());
+    }
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedImageUri = data.getData();
+                questionImageView.setImageURI(selectedImageUri);
+//                int imageWidth = questionImageView.getDrawable().getIntrinsicWidth();
+                int imageHeight = questionImageView.getDrawable().getIntrinsicHeight()/2;
+
+                ViewGroup.LayoutParams layoutParams = questionImageView.getLayoutParams();
+                // Set the ImageView's size based on the image's dimensions
+                layoutParams.height = imageHeight;
+                questionImageView.setLayoutParams(layoutParams);
+            }
+        }
+    }
+}
--- app/src/main/java/tdtu/fit/hrz/flashcards/objects/Flashcard.java
+++ app/src/main/java/tdtu/fit/hrz/flashcards/objects/Flashcard.java
@@ -1,6 +1,33 @@
 package tdtu.fit.hrz.flashcards.objects;
 
+/**
+ * a Flashcard has 2 sides, head and tail
+ * head is for question
+ * tail is for answer
+ */
 public class Flashcard {
 
+    private String question;
+    private String answer;
 
+    public Flashcard(){
+        question = "";
+        answer = "";
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }

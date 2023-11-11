a79ac7857e914d653cf160b136f76b0af11c0d31
Le Phuoc Thinh <phuocthinhokay@gmail.com>
2023-11-12 00:14:25 +0700

    Sync Log in Activity with Firebase Update Main Activity when Log in successfully Update UI for Main/Login/Sign Up Activities

--- app/src/main/java/tdtu/fit/hrz/flashcards/LogInActivity.java
+++ app/src/main/java/tdtu/fit/hrz/flashcards/LogInActivity.java
@@ -1,13 +1,20 @@
 package tdtu.fit.hrz.flashcards;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import tdtu.fit.hrz.flashcards.objects.UserAccount;
 
@@ -16,6 +23,7 @@ public class LogInActivity extends AppCompatActivity {
     EditText usernameEditText;
     TextInputEditText passwordEditText;
     UserAccount demo_account;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +32,56 @@ public class LogInActivity extends AppCompatActivity {
         demo_account = new UserAccount("demoUsername", "demoPassword", "Demo User");
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("UserAccountInfo");
     }
 
-    public void logInConfirmButtonClick(View view) {
+    public void loginConfirmButtonClick(View view) {
         username = usernameEditText.getText().toString();
         password = passwordEditText.getText().toString();
 
-        if (!username.equals(demo_account.getUsername())) {
-            Toast.makeText(this, "Cannot find this username!", Toast.LENGTH_SHORT).show();
-        } else if (!password.equals(demo_account.getPassword())) {
-            Toast.makeText(this, "Wrong password!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Login Successfully!", Toast.LENGTH_SHORT).show();
-        }
+        checkLoginInfo(username, password);
+    }
+
+    private void checkLoginInfo(String username, String password) {
+        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        UserAccount userAccount = snapshot.getValue(UserAccount.class);
+
+                        if (userAccount != null && userAccount.getPassword().equals(password)) {
+                            Toast.makeText(LogInActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                            loginSuccess(userAccount.getDisplayName());
+                        } else {
+                            Toast.makeText(LogInActivity.this, "Incorrect password!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(LogInActivity.this, "Username not found!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(LogInActivity.this, "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void loginSuccess(String displayName) {
+        Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+        intent.putExtra("DISPLAY_NAME", displayName);
+        startActivity(intent);
+        finish();  // Đóng LogInActivity nếu cần
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/tdtu/fit/hrz/flashcards/MainActivity.java
+++ app/src/main/java/tdtu/fit/hrz/flashcards/MainActivity.java
@@ -6,17 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button loginButton, signUpButton;
+    Button loginButton, signUpButton, logoutButton;
+    TextView haveAccountTitle, welcomeUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         signUpButton = findViewById(R.id.signUpButton);
+        haveAccountTitle = findViewById(R.id.haveAccountTitle);
+        loginButton = findViewById(R.id.logInButton);
+        logoutButton = findViewById(R.id.logoutButton);
+        welcomeUser = findViewById(R.id.welcomeUser);
+
+
+        if (getIntent().hasExtra("DISPLAY_NAME")) {
+            String displayName = getIntent().getStringExtra("DISPLAY_NAME");
+            logoutButton.setVisibility(View.VISIBLE);
+            loginButton.setVisibility(View.INVISIBLE);
+            signUpButton.setVisibility(View.INVISIBLE);
+            haveAccountTitle.setVisibility(View.INVISIBLE);
+            welcomeUser.setText("Welcome " + displayName);
+        }
     }
 
     public void signUpButtonClick(View view) {
@@ -27,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     public void loginButtonClick(View view) {
         Intent intent = new Intent(MainActivity.this, LogInActivity.class);
         startActivity(intent);
+        finish();
     }
 
     public void quizLayoutClick(View view) {

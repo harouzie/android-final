d8b4a9b0efdffd12617e99cd60c7a9506cc71812
Kata <khanhtanfts@gmail.com>
2023-11-21 09:41:01 +0700

    - Added spaced repetition algorithm SM-2, anki based in Card.java - not yet useful functions for the feature of the app in Deck.java

--- app/src/main/java/tdtu/fit/hrz/flashcards/objects/Card.java
+++ app/src/main/java/tdtu/fit/hrz/flashcards/objects/Card.java
@@ -1,6 +1,7 @@
 package tdtu.fit.hrz.flashcards.objects;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 /**
  * a Flashcard has 2 sides, head and tail
@@ -13,6 +14,13 @@ public class Card implements Serializable {
     private String question;
     private String answer;
     private boolean flipped = false;
+
+    // Card Review algorithm, if a user download a new deck, these field in those newly downloaded deck
+    // should be the default value or not have these field in the json at all
+    private int repetitions = 0;
+    private int interval = 1;
+    private float ease = 2.5f;
+    private LocalDate nextPractice;
 
     public Card(String question, String answer) {
         this.question = question;
@@ -57,5 +65,36 @@ public class Card implements Serializable {
 
     public void setIndex(int index) {
         this.index = index;
+    }
+
+    //Spaced repetition SM-2, anki based algorithm
+
+    public void updateNextPractice(int rating) {
+        if (rating < 1 || rating > 4) {
+            throw new IllegalArgumentException("Rating should be between 1 and 4");
+        }
+
+        this.ease = Math.max(1.3f, this.ease + (0.1f - (5 - rating) * (0.08f + (5 - rating) * 0.02f)));
+
+        if (rating == 1) {
+            this.repetitions = 0;
+            this.interval = 1;
+        } else if (this.repetitions <= 1) {
+            this.repetitions = this.repetitions + 1;
+            this.interval = rating == 2 ? 1 : 3;
+        } else {
+            this.repetitions = this.repetitions + 1;
+            this.interval = (int) (this.interval * this.ease);
+        }
+
+        this.nextPractice = LocalDate.now().plusDays(this.interval);
+    }
+
+    public boolean isDueDate() {
+        if (nextPractice == null) return true;
+        if (nextPractice.isAfter(LocalDate.now()) || nextPractice.isEqual(LocalDate.now())) {
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/tdtu/fit/hrz/flashcards/objects/Deck.java
+++ app/src/main/java/tdtu/fit/hrz/flashcards/objects/Deck.java
@@ -5,8 +5,10 @@ import android.graphics.drawable.Drawable;
 import androidx.annotation.NonNull;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Deck implements Serializable {
     private String deckName;
@@ -18,7 +20,6 @@ public class Deck implements Serializable {
     private int size;
     private Drawable coverImage;
     private ArrayList<Card> cards;
-
 
     public Deck(@NonNull ArrayList<Card> cards) {
         this.cards = cards;
@@ -97,5 +98,31 @@ public class Deck implements Serializable {
 
     public void setCards(ArrayList<Card> cards) {
         this.cards = cards;
+    }
+
+
+    // these 2 functions currently useless,
+    // cuz it return a list of card objects that didn't linked to the real cards in data
+    public ArrayList<Card> getDueDateCard() {
+        ArrayList<Card> dueDateCards = new ArrayList<>();
+        for (int i = 0; i <= cards.size()-1; i++) {
+            if (cards.get(i).isDueDate()) dueDateCards.add(cards.get(i));
+        }
+        return dueDateCards;
+    }
+
+    public static ArrayList<Card> getDueDateCardFromAllDeck(List<Deck> decks) {
+        ArrayList<Card> dueDateCards = new ArrayList<>();
+
+        for (int i = 0; i <= decks.size()-1; i++) {
+            for (int j = 0; j <= decks.get(i).getSize()-1; j++) {
+                Card card = decks.get(i).getCards().get(j);
+                if (card.isDueDate()) {
+                    dueDateCards.add(card);
+                }
+            }
+        }
+
+        return dueDateCards;
     }
 }
